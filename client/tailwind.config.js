dc62869c63af6108beb8e49625c7f1d5c32cebad
husni <husnovember21@gmail.com>
2025-08-04 12:03:10 +0700

    re-initialize(2) front-end packages

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        '4x1': '2.5rem'
+      }
+    },
   },
   plugins: [],
 }
