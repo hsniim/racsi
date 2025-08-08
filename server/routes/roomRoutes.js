1dfaff3f797f24bb679c3b639de1cf3da1f6b5d6
husni <husnovember21@gmail.com>
2025-08-08 14:47:33 +0700

    feat(backend): modified controllers and routes configuration

--- server/routes/roomRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {  getAllRuangan } = require('../controllers/roomControllers');
-
-router.get('/ruangan', getAllRuangan);
-
-module.exports = router;
--- server/routes/ruanganRoutes.js
+++ server/routes/ruanganRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {  getAllRuangan } = require('../controllers/ruanganControllers');
+const {  getAllRuangan } = require('../controllers/ruanganController');
 
 router.get('/ruangan', getAllRuangan);
 
